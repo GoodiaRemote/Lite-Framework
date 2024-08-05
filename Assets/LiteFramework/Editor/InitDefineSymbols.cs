f1a2332bc205856dd1c8adc3d0860a589b87c5ff
dat <tiendat361999@gmail.com>
2024-08-05 22:08:25 +0700

    update

---
+++ Assets/LiteFramework/Editor/InitDefineSymbols.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace LiteFramework.Editor
+{
+    [InitializeOnLoad]
+    public class InitDefineSymbols
+    {
+        public static readonly string [] Symbols = new string[] {
+            "UNITASK_DOTWEEN_SUPPORT",
+        };
+        
+        static InitDefineSymbols ()
+        {
+            string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup ( EditorUserBuildSettings.selectedBuildTargetGroup );
+            List<string> allDefines = definesString.Split ( ';' ).ToList ();
+            allDefines.AddRange ( Symbols.Except ( allDefines ) );
+            PlayerSettings.SetScriptingDefineSymbolsForGroup (
+                EditorUserBuildSettings.selectedBuildTargetGroup,
+                string.Join ( ";", allDefines.ToArray () ) );
+        }
+    }
+}
